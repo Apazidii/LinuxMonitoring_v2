409a27a30dd7e6b9f38179efbe9a7513a2b80562
dgalactu <Kostya.apazidi@gmail.com>
2023-04-18 10:15:48 +0300

    release

--- 09/main.sh
+++ 09/main.sh
@@ -18,7 +18,7 @@ while true; do
     myex_cpu="$(cat /proc/loadavg | awk '{print $1}')"
     myex_ram="$(free | head -2 | tail -1 | awk '{print $2}')"
     myex_ram_us="$(free | head -2 | tail -1 | awk '{print $3}')"
-    myex_space="$(df /| grep / | awk '{print $2}')"
+    myex_space_avail="$(df /| grep / | awk '{print $4}')"
     myex_space_us="$(df /| grep / | awk '{print $3}')"
     
 
@@ -27,7 +27,7 @@ while true; do
     print_metric "myex_cpu" $myex_cpu
     print_metric "myex_ram" $myex_ram
     print_metric "myex_ram_us" $myex_ram_us
-    print_metric "myex_space" $myex_space
+    print_metric "myex_space_avail" $myex_space_avail
     print_metric "myex_space_us" $myex_space_us
     
     echo "[$(date +%s)] Program working..."
