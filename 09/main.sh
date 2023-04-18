c8c42e1516802fbeef02b0753c6812ef27cd1a68
dgalactu <Kostya.apazidi@gmail.com>
2023-04-18 09:38:48 +0300

    remake 09 v1.001

--- 09/main.sh
+++ 09/main.sh
@@ -10,8 +10,8 @@ then
 fi
 
 function print_metric() {
-    echo "# TYPE $1 gauge" >> $3
-    echo "$1 $2" >> $3
+    echo "# TYPE $1 gauge" >> "$output_file"
+    echo "$1 $2" >> "$output_file"
 }
 
 while true; do
@@ -22,13 +22,12 @@ while true; do
     myex_space="$(df /| grep / | awk '{print $2}')"
     myex_space_us="$(df /| grep / | awk '{print $3}')"
     
-    print_metric "myex_cpu" $myex_cpu $output_file
-    print_metric "myex_ram" $myex_ram $output_file
-    print_metric "myex_ram_us" $myex_ram_us $output_file
-    print_metric "myex_space" $myex_space $output_file
-    print_metric "myex_space_us" $myex_space_us $output_file
+    print_metric "myex_cpu" $myex_cpu
+    print_metric "myex_ram" $myex_ram
+    print_metric "myex_ram_us" $myex_ram_us
+    print_metric "myex_space" $myex_space
+    print_metric "myex_space_us" $myex_space_us
     
-    cat $output_file
-
+    echo "Program working..."
     sleep 3
 done
